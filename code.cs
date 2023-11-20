51e48e1a7a7901a643f56994ac927f44c9ad00ac
Chakraborty <anirbachakrabor7@deloitte.com>
2023-11-21 01:15:21 +0530

    commit

---
+++ code.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyCSharpAPI
+{
+    public class LegacyAPI
+    {
+        public string GetLegacyData()
+        {
+            return "This is legacy data.";
+        }
+
+        public void PerformLegacyOperation()
+        {
+            Console.WriteLine("Performing legacy operation...");
+        }
+    }
+}
